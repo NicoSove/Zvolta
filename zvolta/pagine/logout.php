c446e28a47b27fc27ba30badca0504ea23d9be9e
NicoSove <164329545+NicoSove@users.noreply.github.com>
2025-02-28 13:37:13 +0100

    Delete zvolta directory

--- zvolta/pagine/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // Inizia la sessione
-session_destroy(); // Distrugge la sessione
-header("Location: ../home.php"); // Reindirizza alla homepage o a un'altra pagina
-exit(); // Termina lo script
-?>
