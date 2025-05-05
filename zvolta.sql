f8ad880c3e0f73b6bac4a70939fcab1cbcb5d72c
Massimogreco06 <166703686+Massimogreco06@users.noreply.github.com>
2025-05-05 18:35:04 +0200

    Update zvolta.sql

--- zvolta.sql
+++ zvolta.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Feb 25, 2025 alle 23:20
+-- Creato il: Mag 05, 2025 alle 18:33
 -- Versione del server: 5.7.17
 -- Versione PHP: 5.6.30
 
@@ -21,90 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `zvolta`
 --
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `parcheggio`
---
-
-CREATE TABLE `parcheggio` (
-  `Posto` int(11) NOT NULL,
-  `Stato` varchar(50) NOT NULL,
-  `ID_prenotazione` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `prenotazione`
---
-
-CREATE TABLE `prenotazione` (
-  `ID_prenotazione` int(11) NOT NULL,
-  `Data` date NOT NULL,
-  `contModifiche` int(11) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `posto` varchar(100) DEFAULT NULL,
-  `luogo` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dump dei dati per la tabella `prenotazione`
---
-
-INSERT INTO `prenotazione` (`ID_prenotazione`, `Data`, `contModifiche`, `username`, `posto`, `luogo`) VALUES
-(22, '2025-02-21', 0, 'utente3.0', 'C10', NULL),
-(23, '2025-02-21', 0, 'utente3.0', 'C15', NULL),
-(26, '2025-02-22', 0, 'utente2.0', 'C7', NULL),
-(43, '2025-02-22', 0, 'utente2.0', 'C20', NULL),
-(45, '2025-02-22', 0, 'utente2.0', 'D2', 'A2'),
-(46, '2025-02-25', 0, 'utente2.0', 'C23', 'A2'),
-(48, '2025-02-25', 0, 'utente2.0', 'C15', 'A2'),
-(49, '2025-02-25', 0, 'utente2.0', 'A6', 'A1'),
-(50, '2025-02-25', 0, 'utente2.0', 'A11', 'A1'),
-(51, '2025-02-25', 0, 'utente2.0', 'C7', 'A2'),
-(52, '2025-02-25', 0, 'utente2.0', 'C18', 'A2'),
-(53, '2025-02-25', 0, 'utente3.0', 'D5', 'A2'),
-(54, '2025-02-25', 0, 'utente3.0', 'A14', 'A1'),
-(55, '2025-02-25', 0, 'utente3.0', 'A16', 'A1');
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `salariunioni`
---
-
-CREATE TABLE `salariunioni` (
-  `ID_salaRiunioni` int(11) NOT NULL,
-  `Info` text NOT NULL,
-  `Stato` varchar(50) NOT NULL,
-  `ID_prenotazione` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `scrivania a1`
---
-
-CREATE TABLE `scrivania a1` (
-  `Posto` int(11) NOT NULL,
-  `Stato` varchar(50) NOT NULL,
-  `ID_prenotazione` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `scrivania a2`
---
-
-CREATE TABLE `scrivania a2` (
-  `Posto` int(11) NOT NULL,
-  `Stato` varchar(50) NOT NULL,
-  `ID_prenotazione` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -128,51 +44,19 @@ CREATE TABLE `utente` (
 --
 
 INSERT INTO `utente` (`nome_utente`, `password_utente`, `mail_utente`, `cognome_utente`, `telefono_utente`, `ruolo_utente`, `username`, `ID_coordinatore`) VALUES
-('coor', '$2y$10$8sk2CtgUGtSauWZgU9nIKeSqdvwgMMIeX2FxCmBL2zudZleBiKUX.', 'prova@gmail.com', 'coord', 987654321, 'coordinatore', 'coordinatore1', NULL),
+('sd', '$2y$10$/cbqQmMk91z1OvGb0M4SnOhYCCrUf5MpqtQU/HmTBDupNvxRcskiu', 'efefv@fvf.og', 'esfv', 1234567890, 'coordinatore', 'coor', NULL),
+('coor', '$2y$10$SpARVqJq82czwViUXme97u5P2pttXrCFwo0OzkOqYO3m/3Z/rhLzW', 'prova@gmail.com', 'coord', 987654321, 'coordinatore', 'coordinatore1', NULL),
 ('Damiano', '$2y$10$/mV/Zrr6C93BH4Vpng.OlOiK4TGWoVCyHAg2h6Hj5fXJyCyl1T0KS', 'mia@gmail.com', 'Coccia', 1234567890, 'coordinatore', 'ErFaina', NULL),
-('massimo', '$2y$10$iQdOU2ju4M66zNxcPc54AuRkm3C00MmcMy42xYQpp9KgBet921wsW', 'mia@gmail.com', 'greco', 1234567890, 'admin', 'max11', NULL),
+('fv', '$2y$10$346d10YYEronyUl1qrIcX.xh0xPq3Q5EAFScgvUB4R8FJVuyF53wm', 'sfdv@sdfdfv', 'sdfv', 1323134423, 'admin', 'menager', NULL),
+('sadfvf', '$2y$10$TkH7gaJ8W0vRkCfOT3IHxuesfhiDIwpN9JX44fv3w5J0KxwhSEfMS', 'sfdv@fgtgtt', 'dfv', 234567890, 'admin', 'prova1', NULL),
+('erf', '$2y$10$jLpBfwGNkS2y6yQ4hPbPrOk68F9itFKAQu0O1flsvoLorB8lPBdA6', 'eweffg@fvrtrv', 'ewrg', 2345, 'utente_base', 'UtCoor', 'coor'),
 ('utente', '$2y$10$9kop6vTI1Odgu2ZA30Enruw9ENzcBJbgOnhMlwyeaST/m2IDmX0Cm', 'prova@gmail.com', 'ut', 1234567890, 'utente_base', 'utente1', 'coordinatore1'),
-('utente', '$2y$10$2qq4Dg9UKYvWzsO.JemLWOcBrPy0BanhruKupnhsaHG/.A5cnx8H2', 'sdffvsdf@dsfv.it', 'ut', 1234567890, 'utente_base', 'utente2.0', 'coordinatore1'),
-('u', '$2y$10$2uCvK0CJUi/SGVK2f6il7OnzvKNegKv7APY24x/4qwWyZ0nSTVMYS', 'sdffgdasg@dfgsdffg', 't', 1234567890, 'utente_base', 'utente3.0', 'coordinatore1');
+('utente', '$2y$10$2qq4Dg9UKYvWzsO.JemLWOcBrPy0BanhruKupnhsaHG/.A5cnx8H2', 'sdffvsdf@dsfv.it', 'ut', 1234567890, 'utente_base', 'utente2.0', 'coordinatore1\r\n'),
+('locatelli', '$2y$10$2uCvK0CJUi/SGVK2f6il7OnzvKNegKv7APY24x/4qwWyZ0nSTVMYS', 'sdffgdasg@dfgsdffg', 't', 1234567890, 'utente_base', 'utente3.0', 'coordinatore1');
 
 --
 -- Indici per le tabelle scaricate
 --
-
---
--- Indici per le tabelle `parcheggio`
---
-ALTER TABLE `parcheggio`
-  ADD PRIMARY KEY (`Posto`),
-  ADD KEY `ID_prenotazione` (`ID_prenotazione`);
-
---
--- Indici per le tabelle `prenotazione`
---
-ALTER TABLE `prenotazione`
-  ADD PRIMARY KEY (`ID_prenotazione`),
-  ADD KEY `username` (`username`);
-
---
--- Indici per le tabelle `salariunioni`
---
-ALTER TABLE `salariunioni`
-  ADD PRIMARY KEY (`ID_salaRiunioni`),
-  ADD KEY `ID_prenotazione` (`ID_prenotazione`);
-
---
--- Indici per le tabelle `scrivania a1`
---
-ALTER TABLE `scrivania a1`
-  ADD PRIMARY KEY (`Posto`),
-  ADD KEY `ID_prenotazione` (`ID_prenotazione`);
-
---
--- Indici per le tabelle `scrivania a2`
---
-ALTER TABLE `scrivania a2`
-  ADD PRIMARY KEY (`Posto`),
-  ADD KEY `ID_prenotazione` (`ID_prenotazione`);
 
 --
 -- Indici per le tabelle `utente`
@@ -182,67 +66,8 @@ ALTER TABLE `utente`
   ADD KEY `coordinare_matricola_utente` (`ID_coordinatore`);
 
 --
--- AUTO_INCREMENT per le tabelle scaricate
---
-
---
--- AUTO_INCREMENT per la tabella `parcheggio`
---
-ALTER TABLE `parcheggio`
-  MODIFY `Posto` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT per la tabella `prenotazione`
---
-ALTER TABLE `prenotazione`
-  MODIFY `ID_prenotazione` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
---
--- AUTO_INCREMENT per la tabella `salariunioni`
---
-ALTER TABLE `salariunioni`
-  MODIFY `ID_salaRiunioni` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT per la tabella `scrivania a1`
---
-ALTER TABLE `scrivania a1`
-  MODIFY `Posto` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT per la tabella `scrivania a2`
---
-ALTER TABLE `scrivania a2`
-  MODIFY `Posto` int(11) NOT NULL AUTO_INCREMENT;
---
 -- Limiti per le tabelle scaricate
 --
-
---
--- Limiti per la tabella `parcheggio`
---
-ALTER TABLE `parcheggio`
-  ADD CONSTRAINT `parcheggio_ibfk_1` FOREIGN KEY (`ID_prenotazione`) REFERENCES `prenotazione` (`ID_prenotazione`);
-
---
--- Limiti per la tabella `prenotazione`
---
-ALTER TABLE `prenotazione`
-  ADD CONSTRAINT `prenotazione_ibfk_1` FOREIGN KEY (`username`) REFERENCES `utente` (`username`);
-
---
--- Limiti per la tabella `salariunioni`
---
-ALTER TABLE `salariunioni`
-  ADD CONSTRAINT `salariunioni_ibfk_1` FOREIGN KEY (`ID_prenotazione`) REFERENCES `prenotazione` (`ID_prenotazione`);
-
---
--- Limiti per la tabella `scrivania a1`
---
-ALTER TABLE `scrivania a1`
-  ADD CONSTRAINT `scrivania a1_ibfk_1` FOREIGN KEY (`ID_prenotazione`) REFERENCES `prenotazione` (`ID_prenotazione`);
-
---
--- Limiti per la tabella `scrivania a2`
---
-ALTER TABLE `scrivania a2`
-  ADD CONSTRAINT `scrivania a2_ibfk_1` FOREIGN KEY (`ID_prenotazione`) REFERENCES `prenotazione` (`ID_prenotazione`);
 
 --
 -- Limiti per la tabella `utente`
